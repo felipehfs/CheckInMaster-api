397b4c9d37aa9838fdce9899c2afd842f20f790d
Felipe Henrique Ferreira sousa <felipehfsouza@gmail.com>
2019-07-07 18:43:21 -0300

    Negate/accept invite routes

--- controllers/inviteHandler.js
+++ controllers/inviteHandler.js
@@ -12,6 +12,48 @@ exports.createInvite = async function(req, res) {
         res.status(201).json(newInvite)
     } catch(err) {
         console.error(err)
+        res.status(500).json({ message: err })
+    }
+}
+
+exports.getWaitingInvites = async function(req, res) {
+    try {
+        const invites = await Invite.find({ to: req.user.id, accept: 'ESPERA' }).populate('from', 'username')
+        res.json(invites)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({ message: err })
+    }
+}
+
+exports.acceptInvite = async function(req, res) {
+    try {
+        const invite = await Invite.findOne({ _id: req.body.inviteId})
+        const user = await User.findOne({ _id: req.user.id })
+        const index = user.friends.findIndex(friends => friends === invite.from)
+
+        if (index <  0) {
+            user.friends.push(invite.from)
+            await user.save()
+        }
+
+        invite.accept = "ACEITO"
+        const newInvite = await invite.save()
+        res.status(201).json(newInvite)
+    } catch(err) {
+        console.error(err)
         res.status(500).json({ message: err})
+    }
+}
+
+exports.negateInvite = async function(req, res) {
+    try {
+        const invite = await Invite.findOne({ _id: req.body.inviteId })
+        invite.accept = "RECUSADO"
+        const changedInvite = await invite.save()
+        res.json(changedInvite) 
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: err })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,12 @@ module.exports = app => {
     app.post("/api/register", userHandler.register)
     app.post("/api/login", userHandler.signIn)
 
+    app.post("/api/invites/accept", passport.authenticate('jwt', { session: false}), inviteHandler.acceptInvite)
+    app.post("/api/invites/negate", passport.authenticate('jwt', { session: false}), inviteHandler.negateInvite)
+
+    app.get("/api/invites", passport.authenticate('jwt', { session: false}), inviteHandler.getWaitingInvites)
     app.post("/api/invites", passport.authenticate('jwt', { session: false}), inviteHandler.createInvite)
+    
     app.get("/api/users", passport.authenticate('jwt', {session: false}), userHandler.getUser)
     app.put("/api/users/:id", passport.authenticate('jwt', { session: false}), userHandler.updateUser)
 }
